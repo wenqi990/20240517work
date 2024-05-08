75536dc830761e7e9e4a63815486bf06cb24578c
wenqi990 <ytj168200750@gmail.com>
2024-05-08 15:24:38 +0800

    main

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="header">
+    <Header />
+  </div>
+
+  <RouterView />
+
+</template>
+
+<style scoped lang="scss">
+.header{
+  widows: 100dvw;
+  height: 20dvh;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+// options API 寫法
+export default{
+    // components => 放能在 <template> 中當標籤使用的元件或方法
+    components:{
+        RouterView,
+    }
+}
+</script>
+
+<template>
+    <div class="headerArea">
+    <RouterLink class="routerItem" to="/">Home</RouterLink>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.headerArea{
+  widows: 100%;
+  height: 100%;
+  background: rgb(140, 128, 128);
+
+  .routerItem{
+    color:azure;
+    font-size:24px;
+    margin: 0 1%;
+    text-decoration: none;
+  }
+}
+</style>
