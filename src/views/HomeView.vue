86a94d4dbf588311d9b2396f39ab9032d6b00281
wenqi990 <ytj168200750@gmail.com>
2024-05-12 23:12:07 +0800

    main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,393 @@
 </script>
 
 <template>
-  <img src="/public/imags/2904228873_72bfe6406b_b.jpg" style="width: 100%;border-radius: 20%;">
+  <div class="bg">
+        <img src="/public/imags/景點類圖片/南鯤鯓代天府2.jpg">
+    </div>
+  <img src="/public/imags/Hotel img/台南住宿.jpg" style="width: 100%;height: 80dvh;">
     <div class="about">
-    <h1>This is an about page</h1>
+    <h1>四大圖片連結 四大分頁</h1>
   </div>
-  <div>
+
+  <div class="second area">
+        <div class="secondText">
+            <h1>讓我們一起嗨放美食</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis nobis a odio ad tempora officia nam ullam incidunt ipsam nisi vel, doloremque autem minus dolorum.</p>
+            <div>
     <!-- //bootstrap 效果 -->
-    <button class="btn btn-primary">Click me</button> 
+    <button class="btn btn-primary">所有美食</button> 
+  </div>
+        </div>
+        <img src="/public/imags/景點類圖片/東隆宮文化中心.jpg" alt="圖片">
+    </div>
+
+    <div class="third area">
+      <h1>Title</h1>
+        <div class="cradArea">
+            <div class="card">
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+              <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <div class="fourth area">
+      <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" alt="">
+      <div class="fourthText">
+          <h1>讓我們一起嗨放景點</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+          <button class="btn btn-primary">所有景點</button> 
+      </div>
   </div>
   
+  <div class="fifth area">
+      <h1>Title</h1>
+        <div class="cradArea">
+            <div class="card">
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+              <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <div class="sixth area">
+      <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" alt="">
+      <div class="sixthText">
+          <h1>讓我們一起嗨放旅宿</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+          <button class="btn btn-primary">所有旅宿</button> 
+      </div>
+  </div>
+
+  <div class="seventh area">
+      <h1>Title</h1>
+        <div class="cradArea">
+            <div class="card">
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXXX</a>
+                </div>
+              </div>
+            <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+              <div class="card" >
+                <img src="/public/imags/景點類圖片/南鯤鯓代天府.jpg" class="card-img-top" alt="卡片">
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repudiandae dignissimos aliquid consequuntur veritatis.</p>
+                  <a href="#" class="btn btn-primary">XXXXXX</a>
+                </div>
+              </div>
+        </div>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.bg{
+  position: fixed;
+  bottom: 0;
+  z-index: -999;
+  img{
+    width: 100dvw;
+  }
+}
+
+.second{
+    height: 65dvh;
+    justify-content: space-between;
+    align-items: center;
+    display: flex;
+    background-color: #ffffff;
+    // cursor:url(./素材/ae7a5-03aq0-001.ico),auto ;
+
+    .secondText{
+        width: 40%;
+        height: 100%;
+        padding-top: 5%;
+        padding-left: 3%;
+        color: black;
+
+        h1{
+            padding:5% 3%;
+            font-size: 7dvh;
+        }
+    }
+
+    img{
+        padding-right: 5%;
+        width: 606.6px;
+        height: 400px;
+        border-radius: 20%;
+        transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+    }
+}
+
+.third{
+  margin-top: 5%;
+    height: 75dvh;
+    flex-direction: column;
+    align-items: center;
+    background-color: #ffffff;
+    text-align: center;
+
+    .cradArea{
+        margin-top: 2%;
+        width: 80%;
+        height: 80%;
+        margin-left: 10%;
+        display: flex;
+        justify-content: space-around;
+        position: relative;
+        
+        .card{
+          margin: 1%;
+            width: 30%;
+            height: 90%;
+            box-shadow: 5px 5px 5px rgb(108, 107, 107);
+            transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+            
+            .btn{
+                position: absolute;
+                left: 27%;
+                bottom: 5%;
+                background: rgb(49, 49, 49);
+                border: none;
+                width: 45%;
+                font-size: 18px;
+            }
+        }
+    }
+}
+
+.fourth{
+    height: 70dvh;
+    justify-content: space-between;
+    margin-top: 10%;
+    display: flex;
+    background-color: #ffffff;
+    // cursor:url(/滑鼠標.ico),auto ;
+    .fourthText{
+        width: 50%;
+        height: 500.10px;
+        background:#ffffff;
+        font-size: 25px;
+        padding: 5% 4%;
+        
+    }
+    
+    img{
+        width: 700px;
+        height: 400.6px;
+        border-radius: 20%;
+        box-shadow: 5px 5px 5px rgb(108, 107, 107);
+        transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+    }
+}
+
+.fifth{
+  margin-top: 5%;
+    height: 75dvh;
+    flex-direction: column;
+    align-items: center;
+    background-color: #ffffff;
+    text-align: center;
+
+    .cradArea{
+        margin-top: 2%;
+        width: 80%;
+        height: 80%;
+        margin-left: 10%;
+        display: flex;
+        justify-content: space-around;
+        position: relative;
+        
+        .card{
+          margin: 1%;
+            width: 30%;
+            height: 90%;
+            box-shadow: 5px 5px 5px rgb(108, 107, 107);
+            transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+            
+            .btn{
+                position: absolute;
+                left: 27%;
+                bottom: 5%;
+                background: rgb(49, 49, 49);
+                border: none;
+                width: 45%;
+                font-size: 18px;
+            }
+        }
+    }
+}
+
+.sixth{
+    height: 70dvh;
+    justify-content: space-between;
+    margin-top: 10%;
+    display: flex;
+    background-color: #ffffff;
+    // cursor:url(/滑鼠標.ico),auto ;
+    .sixthText{
+        width: 50%;
+        height: 500.10px;
+        background:#ffffff;
+        font-size: 25px;
+        padding: 5% 4%;
+        
+    }
+    
+    img{
+        width: 700px;
+        height: 400.6px;
+        border-radius: 20%;
+        box-shadow: 5px 5px 5px rgb(108, 107, 107);
+        transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+    }
+}
+
+.seventh{
+  margin-top: 5%;
+    height: 75dvh;
+    flex-direction: column;
+    align-items: center;
+    background-color: #ffffff;
+    text-align: center;
+
+    .cradArea{
+        margin-top: 2%;
+        width: 80%;
+        height: 80%;
+        margin-left: 10%;
+        display: flex;
+        justify-content: space-around;
+        position: relative;
+        
+        .card{
+          margin: 1%;
+            width: 30%;
+            height: 90%;
+            box-shadow: 5px 5px 5px rgb(108, 107, 107);
+            transition: 0.5s;
+        // 鼠標移到按鈕時放大
+        &:hover{
+            scale: 1.05;
+        }
+        // 鼠標移到按鈕時縮小
+        &:active{
+            scale: 0.95;
+        }
+            
+            .btn{
+                position: absolute;
+                left: 27%;
+                bottom: 5%;
+                background: rgb(49, 49, 49);
+                border: none;
+                width: 45%;
+                font-size: 18px;
+            }
+        }
+    }
+}
+</style>
