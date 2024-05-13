d9499cb4b65f8a15c28eaf9749d7b22012aba3fc
ohyabe <a2591212948@gmail.com>
2024-05-13 17:01:20 +0800

    add footer

---
+++ src/views/Footer.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+    <div class="footer">
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .footer{
+        width: 100%;
+        height: 20dvh;
+        background: rgb(140, 128, 128);
+        display: flex;
+        bottom: 0;
+        position: relative;
+    }
+</style>
