e16eee8d2bece5889aa796be348184282ab1bd27
Shiang-Yin <ua011413@gmail.com>
2024-05-16 02:31:27 +0800

    add

--- src/views/景點/Attractions.vue
+++ src/views/景點/Attractions.vue
@@ -582,7 +582,7 @@ export default{
             selectedDistricts: [], //勾選後的資訊放入這裡
             // 2、活動類別
             categorys:[],
-            categoryCheck:[],
+            // categoryCheck:[],
             selectCategory:[],
 
 
@@ -606,17 +606,27 @@ export default{
                 this.tainanAtt = data;
 
                 // 綁定圖片(檔案與圖片直接進行更動綁定)
+                //利用forEach經歷所有的台南資訊(item)
+                // 在每個tainanAtt 的元素加上一個名為imagePath 的屬性，裡面放置我的圖片
+                // this.tainanImg[index]，這裡的index會根據forEach中的index來做相對應的添加
                 this.tainanAtt.forEach((item, index) => {
                     item.imagePath = this.tainanImg[index]
                 })
 
                 // console.log(data);
                 // 提取縣市
+                // map用於將一個陣列轉換至另一個陣列
+                // tainanAtt中提取元素.地址轉移到allDistricts的陣列中
+                // set"刪除重複(儲存唯一的值)"地區
+                // new=>創建新的物件/構造函式
+                // (`...`)展開運算符號，將set中的唯一值壓縮成一個新陣列
                 const allDistricts = data.map(item => item.district);
                 this.districts = [...new Set(allDistricts)];
 
                 // 提取類別
-                // 使用reduce將所有景點收集到一個陣列中,
+                // 使用reduce => 將一個陣列轉換為單個值或結構
+                // category在json檔中是[{category:['類別1','類別2'...]}]
+                // (()=>{},[])這裡的[]是讓acc這個參數初始值設為空陣列，在push入我們要的資訊
                 const allCategory = data.reduce((acc,item) =>{
                     acc.push(...item.category);
                     return acc;
@@ -628,7 +638,9 @@ export default{
                 this.summarys = data.map(item => item.summary);
 
                 // // 更新 totalPages
-                // this.totalPages = Math.ceil(this.filteredAtt.length / 10);
+                // 總頁數與資料數量息息相關,寫在這裡可以確保總頁數的計算是基於資料的實際狀態
+                //  Math.ceil() => 內建函式,有小數時，返回比這個數字大的最小整數(例如:4.5返回5)
+                this.totalPages = Math.ceil(this.filteredAtt.length / 10);
             })
             .catch(error => {
                 console.error('資訊錯誤:', error);
@@ -652,20 +664,30 @@ export default{
             this.tainanSearch = ''; // 搜索框清空時,也跳回A
         },
         // ====================建立翻頁功能===================================
+        // 是檢查給定的索引值是否位於當前頁面的範圍內
+        // index 參數代表的是當前頁面中的相對索引，而不是資料的絕對索引
         page(index){
-            // 當索引值大於等於start=0時，以及 小於end=10時
+            // this.start 和 this.end 分別代表"當前頁面"的起始索引和結束索引
+            // 每頁的索引值都是0~9，共10個索引值
             return index >= this.start && index < this.end;
         },
         // 跳到第一頁
         firstPage(){
-            this.start = 0;
-            this.end = 10;
+            this.start = 0;//當前頁面的起始索引為 0
+            this.end = 10;//當前頁面的結束索引為 10（不包括 10）
             // 將數值綁定頁碼(最前頁是第一頁所以直接等於1)
             this.selectedPage=1;
         },
         // 跳到最後一頁
         lastPage(){
+            // 為了確保在每次需要使用 totalPages 的地方，都能夠使用最新的計算結果
             let totalPages = Math.ceil(this.filteredAtt.length / 10);
+            // 最簡單的舉例:
+            /** 共有45筆資料，每頁有10筆資料
+             *  [(0~9)(10~19)(20~29)...(40~49)]
+             *  45/10=4.5  =>也就是說總共5頁(第5頁有5筆資料)
+             *  所以開始的索引位置是:(總頁數5-1=4頁)*10=(index=)40
+             */
             this.start = (totalPages - 1) * 10;
             this.end = this.filteredAtt.length;
             // 最後一頁就是totalPages，每頁除10的頁碼總數
@@ -673,22 +695,39 @@ export default{
         },
         // 上一頁
         prevPage() {
+            // 第一頁的第一筆資料index絕對是0，所以start == 0
             if (this.start == 0) {
                 alert("已經是第一頁了!")
             }else{
-                this.start -= 10;
-                this.end -= 10;
+                // 假設:第二頁index=(10~19)，-10變成(0~9)(第一頁)
+                // this.start -= 10;
+                // this.end -= 10;
+                // 使用上面這個方法會有一個bug就是當你的最後一頁index未滿10個時，你點選上一頁，只會出現相同索引值而已
+
+                // 計算前一頁的 start 和 end
+                // 如果this.start - 10大於等於0 =>返回this.start - 10
+                // 如果小於0  => 則返回 0 (也就是第一筆資料)
+                const newStart = Math.max(0, this.start - 10);
+                // 如果this.start 小於等於 this.end  =>則值等於this.start
+                // this.start  大於 this.end  =>則值等於this.end
+                const newEnd = Math.min(this.start, this.end);
+                
+                // 更新 start 和 end
+                this.start = newStart;
+                this.end = newEnd;
+
                 // 往前一頁為減一
                 this.selectedPage--;
             }
         },
-        // 下一頁，this.end是指當前頁結束的索引位置;this.tainanAtt.length這是指所有景點的總數量
+        // this.tainanAtt.length這是指所有景點的總數量
         // 如果為true才會繼續顯示下一頁
         nextPage() {
+            // 不用" == " 是因為，資料的筆數不是每頁都固定
             if (this.end >= this.filteredAtt.length  ) {
                 alert("此為最後一頁")
             }else{
-                // this.start這裡的start是指當前的索引位置
+                //  假設:第二頁index=(10~19)，+10變成(20~29)(第三頁)
                 this.start += 10;
                 this.end += 10;
                 // 下一頁為加一
@@ -702,7 +741,10 @@ export default{
         this.end = Math.min(this.start + 10, this.filteredAtt.length);
         },
         // 更新下拉選單的頁碼(做連結)
+        // even => 當事件發生時(值的改變)，就會將傳送給even做處理
         updateSelectedPage(event) {
+            // 下拉選單中(v-model綁定)，當我點選值時會回傳回來這裡做處理
+            // parseInt() 函式將返回該數字的"整數值"
             this.selectedPage = parseInt(event.target.value);
             this.gotoSelectedPage();
         },
@@ -711,31 +753,41 @@ export default{
         // 針對縣市
         checkDistrict() {
             // 點及按鈕，獲取勾選區域
-            //使用[...]=>擴展運算，創建新的數據，不直接使用原數據
-            // 這裡的目的是為了，確保selectedDistricts為獨立數據，不受modelCheck影響
+            //使用[...]=>擴展運算，創建新的數據，不直接使用原數據，這個新陣列與原始陣列具有相同的值，但是在記憶體中是獨立的
+            // 雖然selectedDistricts數據與modelCheck相同，但當你對 this.selectedDistricts 進行修改時，不會影響到 this.modelCheck，反之亦然。(因為記憶體是獨立的)
             this.selectedDistricts = [...this.modelCheck];
             this.filteredAtt();
         },
         // 針對類別
         checkCategory(){
-            this.categoryCheck=[...this.selectCategory];
+            // this.categoryCheck=[...this.selectCategory];
+            this.selectCategory;
             this.filteredAtt();
-            // console.log(this.selectCategory);
         },
+        // ===========================================
         // 介紹與總結
         toggleArea(index) {
-            if ( this.activeIndex === index &&this.areaBlock === '222') {
+            // this.activeIndex 為被點擊的景點的索引位置 index
+            if ( this.activeIndex === index) {
             // 如果點擊的是當前啟動的景點區域，並且當前顯示的是簡介區域，則切換回景點區域
             this.areaBlock = '111';
-            } else {
+            } 
+            else {
                 // 否則，設置啟動的索引和顯示簡介區域
                 this.activeIndex = index;
                 this.areaBlock = '222';
             }
         },
+        
+        // 返回上一頁
         goBack() {
             this.areaBlock = '111'; // 切回景點區域
-            this.activeIndex = null; // 重置索引值
+            this.activeIndex = null; //null 清除已經啟動的景點索引
+            // 如果不重置就回不去上一個了....
+            /**當你將變數設置為 null 時，表示這個變數不持有任何有效的值。
+             * 這在某些情況下很有用，特別是當你需要清除一個變數的值時，
+             * 或者在應用程式的邏輯中需要表示某個物件或值不存在的時候。
+             * */
         }
         
     },
@@ -748,41 +800,47 @@ export default{
     computed:{
         // 搜索資料
         filteredAtt(){
-            // if ((!this.tainanSearch || this.tainanSearch.length < 2 )&&this.selectedDistricts.length === 0 && this.selectCategory.length===0) {
-            //     return this.tainanAtt;
-            // } else {
-            //     return this.tainanAtt.filter((item) => {
-            //         const searchMatch= (
-            //             item.name.includes(this.tainanSearch) ||
-            //             item.address.includes(this.tainanSearch) ||
-            //             item.category.includes(this.tainanSearch) ||
-            //             item.district.includes(this.tainanSearch) 
-            //         );
-            //         const districtMatch = (
-            //             this.selectedDistricts.length === 0 || 
-            //              //如果沒有選擇，返回true，匹配所有景點
-            //             this.selectedDistricts.includes(item.district) 
-            //              //如果選擇了地點，只返回選擇地點的景點
-            //         );
-            //         const categoryMath=(
-            //             this.selectCategory.length===0||
-            //             this.selectCategory.includes(item.category)
-            //         )
-            //         // 返回搜索匹配和地區匹配以及類型匹配，為true的景点
-            //         return searchMatch && districtMatch && categoryMath;
-            //     })
-            // }
             return this.tainanAtt.filter((item) => {
+                // v-model=' tainanSearch'，僅會針對 this.tainanAtt 中每個元素的 name 和 address 屬性進行篩選
                 const nameMatch = item.name.includes(this.tainanSearch);
                 const addressMatch = item.address.includes(this.tainanSearch);
+
+                // 當欄位[未勾選]==0 
+                /**當欄位[有勾選時]  =>檢查 item.district 是否包含在 this.selectedDistricts 中
+                *   如果是，則返回 true，表示該景點所在的行政區符合選擇的行政區之一。如果不是，則返回 false
+                */
+                /**   ========重要的地方==============
+                 *      使用` || `
+                 *  其中一個表達式為 true，則整個表達式的結果將為 true；
+                 *  只有在兩個表達式都為 false 時，整個表達式的結果才為 false
+                 */
                 const districtMatch = this.selectedDistricts.length === 0 || this.selectedDistricts.includes(item.district);
+
+                /**  =========includes與some============
+                 *  includes:整個陣列中是否包含指定的值。但是，它不能檢查多個值是否包含在陣列中。
+                 *  some:用於檢查陣列中是否至少有一個元素滿足指定的條件。
+                 *  以下面為例:category:['類型1','類型2','類型3'...]
+                 *  因為category 包含很多類型，如果使用includes，他會將每個 item.category 的值分別與 this.selectCategory 進行比對。
+                 * 這在類型很多的情況下會變得複雜且效率較低。
+                 */
+                //some 方法期望的是一個函數作為參數(some的參數是一個回調函數)，而不是一個陣列。
                 const categoryMatch = this.selectCategory.length === 0 || this.selectCategory.some(cat => item.category.includes(cat));
 
                 // 同時考慮搜索框和多選框的輸入
+                // 要確保所有篩選條件都滿足才會回傳
+                // 上面的地址和類別因為都用 ‵ || ‵  ，所以只要成立就都是true
+                // 這是讓他可以做多重選擇相互連通的關鍵
                 return (nameMatch || addressMatch) && districtMatch && categoryMatch;
             });
         },
         totalPagesArray(){
+            /**Array.from()  是一個靜態方法，他可以用於
+                1、類陣列物件（比如具有 length 屬性的物件）或者是一個可迭代物件（比如一個 Set、Map、String 等）
+                2、映射函式（選擇性）：一個函式，用於對陣列中的每個元素進行映射。這個函式接受三個參數：currentValue（當前元素的值）、index（當前元素的索引）、array（原始陣列或物件）。這個參數是選擇性的，你可以根據需要省略它。
+            */
+            // { length: this.totalPages } 指"頁面的總數"
+            // (_, index)  ` ＿ ` =>用來忽略第一個參數(陣列中的函示 例如常用(item,index)然後忽略item)
+            // index + 1   通常頁碼都是從第一頁開始，(所以index變成1~n)
             return Array.from({ length: this.totalPages }, (_, index) => index + 1);
         }
     },
@@ -796,11 +854,11 @@ export default{
             },
             // immediate: true 表示在監聽開始時立即調用處理函數
             immediate: true
-            },
         },
-        components: { Footer },
-    setup(){
-    return {}
+    },
+    components: { Footer },
+        setup(){
+        return {}
     }
 
 }
@@ -818,37 +876,41 @@ export default{
 
     <div class="big">      
 
-        <div class="leftArea">
-            <!-- 搜索 -->
-            <!-- 逐步解析:
-                1、lazy:在更新數據時使用，用於"輸入時，值並不會立馬更新"(需要特定按鈕才會開始更新"例如enter")
-                2、trim:自動刪除值的前後空格
-                @:做監聽事件
-                3、@input=""  :方法中做監聽，也就是輸入時做searchAll這個涵式
-                4、@clear=""  :clear是自訂義事件，指清除...，這邊再次做確定(清除搜索結果或重置搜索狀態=>讓她確保可以A時顯示資訊)
+        <div class="leftBigArea">
+            <div class="leftArea">
+                <!-- 搜索 -->
+                <!-- 逐步解析:
+                    1、lazy:在更新數據時使用，用於"輸入時，值並不會立馬更新"(需要特定按鈕才會開始更新"例如enter")
+                    2、trim:自動刪除值的前後空格
+                    @:做監聽事件
+                    3、@input=""  :方法中做監聽，也就是輸入時做searchAll這個涵式
+                    4、@clear=""  :clear是自訂義事件，指清除...，這邊再次做確定(清除搜索結果或重置搜索狀態=>讓她確保可以A時顯示資訊)
 
-                搜索欄只設定可搜索景點名稱和地址
-            -->
-            <input class="searchArea" id="searchArea" v-model.lazy.trim="tainanSearch" @input="searchAll"  @clear="cancelSearch" type="search" placeholder="請輸入欲搜索景點(至少兩個字)">
-            <!-- 綁定json檔中的districts，將地址前都附上多選框 -->
-            <!-- 利用v-for來遍歷districts  -->
-            <p class="textP">縣市:</p>
-            <div class="checkArea" v-for="district in districts" :key="district">
-                <!-- 並附上複選框 -->
-                <!-- 每個複選框接有一個v-model==>也就是，將資訊綁定到上面，
-                當複選框被選中或取消時,modelCheck都會改變
+                    搜索欄只設定可搜索景點名稱和地址
                 -->
-                <label  :for="'checkbox-' + district">{{ district }}</label>
-                <input class="checkbox checkboxText" id="'checkbox-'+district" type="checkbox" v-model="selectedDistricts" :value="district" name="">
-            </div>
+                <input class="searchArea" id="searchArea" v-model.lazy.trim="tainanSearch" @input="searchAll"  @clear="cancelSearch" type="search" placeholder="請輸入欲搜索景點(至少兩個字)">
+
+                <!-- 綁定json檔中的districts，將地址前都附上多選框 -->
+                <!-- 利用v-for來遍歷districts  -->
+                <p class="textP">縣市:</p>
+                <div class="checkArea" v-for="district in districts" :key="district">
+                    <!-- 並附上複選框 -->
+                    <!-- 每個複選框接有一個v-model==>也就是，將資訊綁定到上面，
+                    當複選框被選中或取消時,modelCheck都會改變
+                    -->
+                    <label  :for=" district">{{ district }}</label>
+                    <input class="checkbox checkboxText" id="district" type="checkbox" v-model="selectedDistricts" :value="district" name="">
+                </div>
 
 
-            <p class="textP">活動類型:</p>
-            <div class="checkArea" v-for="category in categorys" :key="category">
-                <label  :for="'checkbox2-' + category">{{ category }}</label>
-                <input class="checkbox2 " id="'checkbox2-'+district" type="checkbox" v-model="selectCategory" :value="category" name="">
+                <p class="textP">活動類型:</p>
+                <div class="checkArea" v-for="category in categorys" :key="category">
+                    <label  :for="category">{{ category }}</label>
+                    <input class="checkbox2 " id="category" type="checkbox" v-model="selectCategory" :value="category" name="">
+                </div>
             </div>
         </div>
+
 
         <div class="rightArea">
 
@@ -875,7 +937,7 @@ export default{
                         
                         <!-- ================================================================== -->
                     
-                <!-- </div> -->
+                <!-- </div>activeIndex === index  -->
 
                             <!-- 當areaBlock==222 -->
                             <div class="introductionArea" v-show="activeIndex === index  ">
@@ -956,8 +1018,15 @@ export default{
     justify-content: left;
 }
 
-.leftArea{
+.leftBigArea{
     width: 20%;
+    height: 210dvh;
+    margin-left: 1%;
+    margin-top: 1%;
+}
+
+.leftArea{
+    width: 100%;
     height: 100dvh;
     display: flex;
     flex-wrap: wrap;
