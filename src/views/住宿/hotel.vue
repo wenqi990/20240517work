64a4b7e52bca2bcd5593fe2bf3234173069b865f
ohyabe <a2591212948@gmail.com>
2024-05-15 14:48:22 +0800

    add footer

--- src/views/住宿/hotel.vue
+++ src/views/住宿/hotel.vue
@@ -1,5 +1,5 @@
-<script>
-
+<script >
+import Footer from '../../views/Footer.vue';
 
 export default {
     data() {
@@ -124,6 +124,10 @@ export default {
             return this.filteredHotels.slice(start, end); // 它使用 slice() 方法從篩選後的飯店資料陣列 filteredHotels 中取出從 start 到 end 範圍的飯店資料，然後將這個範圍的資料作為結果返回。.slice(start, end) 是 JavaScript 陣列的一個方法，用於從陣列中選擇一個子集，並返回一個新的陣列，包含從開始索引 (start) 到結束索引 (end) 之間的元素（但不包括結束索引的元素）。
         },
     },
+    components: { Footer },
+    setup(){
+    return {}
+    }
 }
 </script>
 
@@ -190,36 +194,8 @@ export default {
 
     <!-- <div class="footerArea"></div> -->
     
-    <div class="footer">
-        <div class="main">
 
-    
-<div class="icon fb" onclick="location.href='https://www.facebook.com/';">
-    <i class="pi pi-facebook"></i>
-    <span>Facebook</span>
-</div>
-<div class="icon twt" onclick="location.href='https://twitter.com/';">
-    <i class="pi pi-twitter"></i>
-    <span>Twitter</span>
-</div>
-
-<div class="icon ig" onclick="location.href='https://www.instagram.com/';">
-    <i class="pi pi-instagram"></i>
-    <span>Instagram</span>
-</div>
-
-<div class="icon discord" onclick="location.href='https://discord.gg/';">
-    <i class="pi pi-discord"></i>
-    <span>Discord</span>
-</div>
-
-<div class="icon yt" onclick="location.href='https://www.youtube.com/';">
-    <i class="pi pi-youtube"></i>
-    <span>YouTube</span>
-</div>
-</div>
-
-    </div>
+    <Footer></Footer>
 </template>
 
 <style scoped lang="scss">
@@ -355,87 +331,4 @@ h1 {
     }
 }
 
-.footer{
-        width: 100%;
-        height: 20dvh;
-        background: rgb(192, 183, 183);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        bottom: 0;
-        position: relative;
-   
-
-    
-.main{
-    /* width: 100%;
-    height: 100vh; */
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* background: rgb(240, 240, 240); */
-}
-
-.icon{
-    width: 50px;
-    height: 50px;
-    border-radius: 100px;
-    background: #fff;
-    margin: 20px;
-    text-align: center;
-    font-size: 25px;
-    line-height: 50px;
-    overflow: hidden;
-    box-shadow: 5px 10px 20px rgba(150, 150, 150, .3);
-    transition: all .3s ease-out;
-}
-.icon:hover{
-    width: 200px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    color: #fff;
-}
-
-.icon:hover i{
-    color: #fff;
-   
-}
-.icon .pi pi-facebook{
-    color: #1A6ED8;
-
-}
-.fb:hover{
-    background: #1A6ED8;
-
-    
-}
-.icon .pi pi-twitter{
-    color: #1DA1F2;
-}
-.twt:hover{
-    background: #000000;
-}
-.icon .pi pi-instagram{
-    color: #ef55b9;
-}
-.ig:hover{
-    background: rgb(2,0,36);
-    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(136,29,227,1) 0%, rgba(187,29,183,1) 12%, rgba(255,206,0,1) 100%);
-}
-.icon .pi pi-discord{
-    color: #000;
-}
-.discord:hover{
-    background: rgb(114,137,218);
-}
-.icon .pi pi-youtube{
-    color: #FE0000;
-}
-.yt:hover{
-    background: #FE0000;
-}
-
-}
 </style>
